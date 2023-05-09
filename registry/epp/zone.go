d7b3ec6e3626581cdd09a0e3cfc557771fca3dbf
Roman <gauzy.gossamer@gmail.com>
2023-05-09 11:33:17 +0300

    +getRegistrarZones

--- registry/epp/zone.go
+++ registry/epp/zone.go
@@ -25,6 +25,27 @@ func getDomainZone(db *server.DBConn, domain string) *Zone {
     return &zone_obj
 }
 
+func getRegistrarZones(db *server.DBConn, regid uint) ([]string, error) {
+    rows, err := db.Query("SELECT fqdn FROM registrarinvoice r JOIN zone z on r.zone=z.id " +
+                       "WHERE registrarid = $1::integer and " +
+                       "todate is null and fromdate <= now()" , regid)
+    var zones []string
+    if err != nil {
+        return zones, err
+    }
+
+    for rows.Next() {
+        var zone string
+        err := rows.Scan(&zone)
+        if err != nil {
+            return zones, err
+        }
+        zones = append(zones, zone)
+    }
+
+    return zones, nil
+}
+
 func testRegistrarZoneAccess(db *server.DBConn, regid uint, zoneid int) bool {
     row := db.QueryRow("SELECT id FROM registrarinvoice " +
                        "WHERE registrarid = $1::integer and zone = $2::integer and " +
