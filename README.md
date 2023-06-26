7548d211059e0817a06abc0e73226ded46c6847d
gauzy-gossamer <gauzy.gossamer@gmail.com>
2023-06-27 01:17:21 +0300

    updated README with test epp_client

--- README.md
+++ README.md
@@ -9,8 +9,20 @@ The database schema is based on FRED registry (https://github.com/CZ-NIC/fred-db
 ```sh
 # create db schema (sql/init.sql) and copy configuration files
 ./prepare_docker.sh
+```
+
+By default database schema is generated with two registrars (SYSTEM-REG, TEST-REG) and a test zone (ex.com). Use "./build_db_schema.sh > sql/init.sql" to generate schema with predefined registrars and zones.
+
+```sh
 # run modules in docker containers
-docker-compose up -d
+$ docker-compose up -d
+
+$ docker ps
+CONTAINER ID   IMAGE                   COMMAND                  CREATED              STATUS                        PORTS                                                                                      NAMES
+8f69c96c517c   registry_regcore        "/usr/src/registry/r…"   About a minute ago   Up About a minute             0.0.0.0:8090->8090/tcp, :::8090->8090/tcp, 0.0.0.0:51015->51015/tcp, :::51015->51015/tcp   regcore
+ee2b4a51c33e   registry_regadmin       "uvicorn main:app --…"   About a minute ago   Up About a minute (healthy)   0.0.0.0:8088->8088/tcp, :::8088->8088/tcp                                                  regadmin
+6c6cd0386e44   registry_regwhois       "/usr/src/whois/whois"   About a minute ago   Up About a minute (healthy)   0.0.0.0:8043->8043/tcp, :::8043->8043/tcp                                                  regwhois
+eb1b9f569788   postgres:latest         "docker-entrypoint.s…"   About a minute ago   Up About a minute (healthy)   0.0.0.0:7432->5432/tcp, :::7432->5432/tcp                                                  registry_regdb_1
 ```
 
 # Configuration
@@ -47,10 +59,19 @@ admin_cli.py --zone-set-price --zone example.com --operation "TransferDomain" --
 admin_cli.py --create-registrar --handle TEST-REG --name "Test Registrar" 
 ```
 ### Generate certificate
+
+Use script cert/gen_registrar_cert.sh to generate registrar certificates.
+Note that for production environment you need to generate CA certificates first with gen_ca_cert.sh command.
+
 ```sh
-./cert/gen_registrar_cert.sh TEST-REG
+# this will create files client/TEST-REG.crt client/TEST-REG.csr client/TEST-REG.key
+$ ./gen_registrar_cert.sh TEST-REG
+...
+TEST-REG md5 Fingerprint=05:3D:AE:11:5C:CC:A6:C1:08:61:24:E1:EA:12:67:50
 ```
+
 ### Set registrar ACL
+Set up access parameters. Use md5 fingerprint from the generated certificate as CERT_FINGERPRINT.
 ```python
 admin_cli.py --set-registrar-acl --handle TEST-REG --cert CERT_FINGERPRINT --password password
 ```
@@ -58,4 +79,174 @@ admin_cli.py --set-registrar-acl --handle TEST-REG --cert CERT_FINGERPRINT --pas
 ### Give access to use zones
 ```python
 admin_cli.py --registrar-add-zone --handle TEST-REG --zone example.com
+```
+
+## Regular tasks
+
+Set up these jobs with crontab.
+
+### Zone generation
+
+This command will create a file ex.com.db with a generated DNS zone:
+
+```python
+cd scripts && python3 generate_zone.py --zone ex.com
+```
+
+### Deletion of expired domains
+
+Script connects to registry core using gRPC service (by default 127.0.0.1:51015).
+
+```sh
+cd scripts && python3 remove_expired_domains.py
+```
+
+## Test EPP Client
+
+Install EPPy module:
+```sh
+git clone https://github.com/gauzy-gossamer/eppy ./
+cd eppy && python3 setup.py install
+```
+
+Now you can use python EPP client to test how registry works:
+
+```python
+# create client
+>>> import scripts.epp_client as epp_client
+>>> client = epp_client.create_epp_client('TEST-REG', 'password', cert='cert/client/test-client.crt', key='cert/client/test-client.key', server='localhost', port=8090)
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <command>
+    <login>
+      <clID>TEST-REG</clID>
+      <pw>password</pw>
+      <options>
+        <version>1.0</version>
+        <lang>en</lang>
+      </options>
+      <svcs>
+        <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
+        <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
+        <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
+      </svcs>
+    </login>
+  </command>
+</epp>
+
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <response>
+    <result code="1000">
+      <msg>Command completed successfully</msg>
+    </result>
+    <trID>
+      <clTRID>2nteQ20vWyF6</clTRID>
+      <svTRID>SV-XpqwEpBUTH</svTRID>
+    </trID>
+  </response>
+</epp>
+
+# create test contact
+>>> contact_info = {
+...     'intPostalInfo':{'name':'John Snow'},
+...     'locPostalInfo':{'name':'John Snow','address':'Winterfell'},
+...     'birthday':'2000-01-01',
+...     'passport':['passport info'], 
+...     'voice':['+1 900 0000'],
+...     'fax': ['+1 900 0001'],
+...     'email':['john.snow@gmail.com'],
+... }
+>>> client.create_contact('test-person', contact_type='person', fields=contact_info)
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <command>
+    <create>
+      <contact:create xmlns:contact="http://www.ripn.net/epp/ripn-contact-1.0" xmlns="http://www.ripn.net/epp/ripn-contact-1.0">
+        <id>test-person</id>
+        <person>
+          <intPostalInfo>
+            <name>John Snow</name>
+          </intPostalInfo>
+          <locPostalInfo>
+            <name>John Snow</name>
+            <address>Winterfell</address>
+          </locPostalInfo>
+          <birthday>2000-01-01</birthday>
+          <passport>passport info</passport>
+          <voice>+1 900 0000</voice>
+          <fax>+1 900 0001</fax>
+          <email>john.snow@gmail.com</email>
+        </person>
+      </contact:create>
+    </create>
+  </command>
+</epp>
+
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <response>
+    <result code="1000">
+      <msg>Command completed successfully</msg>
+    </result>
+    <resData>
+      <contact:creData xmlns:contact="http://www.ripn.net/epp/ripn-contact-1.0" xmlns="http://www.ripn.net/epp/ripn-contact-1.0">
+        <id>test-person</id>
+        <crDate>2023-06-26T20:56:37Z</crDate>
+      </contact:creData>
+    </resData>
+    <trID>
+      <clTRID>yLZuHKZNFweN</clTRID>
+      <svTRID>SV-pKhqHKtswC</svTRID>
+    </trID>
+  </response>
+</epp>
+
+# create test domain
+>>> client.create_domain('domain.ex.com', registrant='test-person')
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <command>
+    <create>
+      <domain:create xmlns:domain="http://www.ripn.net/epp/ripn-domain-1.0" xmlns="http://www.ripn.net/epp/ripn-domain-1.0">
+        <name>domain.ex.com</name>
+        <period unit="y">1</period>
+        <registrant>test-person</registrant>
+      </domain:create>
+    </create>
+  </command>
+</epp>
+
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <response>
+    <result code="1000">
+      <msg>Command completed successfully</msg>
+    </result>
+    <resData>
+      <domain:creData xmlns:domain="http://www.ripn.net/epp/ripn-domain-1.0" xmlns="http://www.ripn.net/epp/ripn-domain-1.0">
+        <name>domain.ex.com</name>
+        <crDate>2023-06-26T20:58:38Z</crDate>
+        <exDate>2024-06-26T20:58:38Z</exDate>
+      </domain:creData>
+    </resData>
+    <trID>
+      <clTRID>5V4Uj32OMDth</clTRID>
+      <svTRID>SV-EgGCcGzMmF</svTRID>
+    </trID>
+  </response>
+</epp>
+
+# logout
+>>> client.logout()
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <command>
+    <logout />
+  </command>
+</epp>
+
+<epp xmlns="http://www.ripn.net/epp/ripn-epp-1.0">
+  <response>
+    <result code="1500">
+      <msg>Command completed successfully; ending session</msg>
+    </result>
+    <trID>
+      <svTRID>SV-yOGCVDZdol</svTRID>
+    </trID>
+  </response>
+</epp>
 ```
---
+++ scripts/epp_client.py
@@ -0,0 +1,312 @@
+import copy
+import time
+
+from eppy.client import EppClientHTTP
+import eppy.doc as ec
+import xmltodict
+
+class RipnEpp():
+    def __init__(self, client, print_commands=True, nsmap_url='http://www.ripn.net', registrar=None, timing=False) -> None:
+        self.nsmap_url = nsmap_url
+        self.extra_nsmap = {
+            '':'{}/epp/ripn-epp-1.0'.format(nsmap_url),
+            'epp':'{}/epp/ripn-epp-1.0'.format(nsmap_url),
+            'domain':'{}/epp/ripn-domain-1.0'.format(nsmap_url),
+            'contact':'{}/epp/ripn-contact-1.0'.format(nsmap_url),
+            'host':'{}/epp/ripn-host-1.0'.format(nsmap_url),
+            'registrar':'{}/epp/ripn-registrar-1.0'.format(nsmap_url)
+        }
+
+        self.authinfo_transfer = False
+        self.session_open = False
+        self.timing = timing
+
+        self.client = client
+        self.registrar = registrar
+        self.print_commands = print_commands
+
+        # order of fields in xml queries, so that they pass schema validation
+        contact_order = ('intPostalInfo', 'locPostalInfo', 'legalInfo', 'taxpayerNumbers', 'birthday', 'passport', 'voice', 'fax', 'email', 'authInfo', 'disclose')
+        postal_order = ('name', 'org', 'address')
+        person_order = {'__order':contact_order, 'intPostalInfo':{'__order':postal_order}, 'locPostalInfo':{'__order':postal_order}}
+        org_order = {'__order':contact_order, 'intPostalInfo':{'__order':postal_order}, 'locPostalInfo':{'__order':postal_order}, 'legalInfo':{'__order':postal_order}}
+
+        ec.EppCreateContactCommand._childorder['__order'] = ('id', 'person', 'organization', 'verified', 'unverified', 'authInfo')
+        ec.EppCreateContactCommand._childorder['organization'] = org_order
+        ec.EppCreateContactCommand._childorder['person'] = person_order
+
+        ec.EppUpdateContactCommand._childorder['__order'] = ('id', 'add', 'rem', 'chg')
+        ec.EppUpdateContactCommand._childorder['chg']['organization'] = org_order
+        ec.EppUpdateContactCommand._childorder['chg']['person'] = org_order
+
+        ec.EppCreateDomainCommand._childorder['__order'] = ('name', 'period', 'ns', 'registrant', 'description', 'authInfo')
+        ec.EppUpdateDomainCommand._childorder['__order'] = ('name', 'add', 'rem', 'chg')
+
+#        ec.EppUpdateRegistrarCommand._childorder['chg'] = {'__order':('voice', 'fax', 'www', 'whois')}
+
+    def set_print_commands(self, print_commands: bool) -> str:
+        self.print_commands = print_commands
+
+    def set_registrar(self, registrar: str) -> None:
+        self.registrar = registrar
+
+    def set_authinfo_transfer(self, authinfo_transfer: bool) -> None:
+        self.authinfo_transfer = authinfo_transfer
+
+    def _client_send(self, cmd) -> dict:
+        start_tm = time.time()
+        if self.print_commands:
+            print("command:")
+            print(cmd)
+        ret = self.client.send(cmd, extra_nsmap=self.extra_nsmap)
+        if self.timing:
+            print('time = {}'.format(time.time() - start_tm))
+        if self.print_commands:
+            print(ret)
+        return xmltodict.parse(str(ret))
+
+    def hello(self):
+        cmd = ec.EppHello(extra_nsmap=self.extra_nsmap)
+
+        return self._client_send(cmd)
+
+    def login(self, clid: str, pw: str, lang='en', clTRID: str=None) -> dict:
+        cmd = ec.EppLoginCommand(extra_nsmap=self.extra_nsmap, lang=lang)
+
+        if clTRID is not None:
+            cmd.add_clTRID(clTRID)
+
+        cmd.clID = clid
+        cmd.pw = pw
+
+        ret = self._client_send(cmd)
+        self.session_open = True
+        return ret
+
+    def logout(self) -> dict:
+        cmd = ec.EppLogoutCommand(extra_nsmap=self.extra_nsmap)
+        return self._client_send(cmd)
+
+    def poll(self, op='req', msg:str = None) -> dict:
+        cmd = ec.EppPollCommand(op, msgID=msg, extra_nsmap=self.extra_nsmap)
+        return self._client_send(cmd)
+
+    def info_contact(self, client_handle: str, authinfo=None) -> dict:
+        cmd = ec.EppInfoContactCommand(extra_nsmap=self.extra_nsmap)
+        cmd.id = client_handle
+
+        if authinfo is not None:
+            cmd.authInfo = {'pw':authinfo}
+
+        return self._client_send(cmd)
+
+    def _set_default(self, fields, field_name, def_value):
+        if fields is not None and field_name in fields:
+            if type(fields[field_name]) is type(def_value):
+                return fields[field_name]
+            else:
+                raise Exception('types dont match {} {}'.format(type(fields[field_name]), type(def_value)))
+        return def_value
+
+    def create_contact(self, client_handle: str, contact_type='org', authinfo=None, fields=None) -> dict:
+        cmd = ec.EppCreateContactCommand(extra_nsmap=self.extra_nsmap)
+        cmd.id = client_handle
+
+        if contact_type == 'org':
+            cmd.organization = fields
+        else:
+            cmd.person = fields
+        if authinfo is not None:
+            cmd.authInfo = {'pw':authinfo}
+
+        if fields is not None and 'verified' in fields and fields['verified']:
+            cmd.verified = True
+
+        return self._client_send(cmd)
+
+    def update_contact(self, client_handle: str, contact_type='org', authinfo=None, add_status=None, rem_status=None, fields=None) -> dict:
+        cmd = ec.EppUpdateContactCommand(extra_nsmap=self.extra_nsmap)
+        cmd.id = client_handle
+        person = {}
+        if fields is not None:
+            person = fields
+
+        if contact_type == 'org':
+            cmd.chg = {'organization':person}
+        else:
+            cmd.chg = {'person':person}
+
+        if authinfo is not None:
+            cmd.chg.authInfo = {'pw':authinfo}
+
+        if add_status is not None:
+            cmd.add = {'status':[{'@s':'clientUpdateProhibited'}]}
+        if rem_status is not None:
+            cmd.rem = {'status':[{'@s':'clientUpdateProhibited'}]}
+
+        return self._client_send(cmd)
+
+    def delete_contact(self, client_handle: str) -> dict:
+        cmd = ec.EppDeleteContactCommand(extra_nsmap=self.extra_nsmap)
+        cmd.id = client_handle
+        return self._client_send(cmd)
+
+    def _idna_domain(self, domain):
+        domain = domain.lower()
+
+        if type(domain) is str: #unicode:
+            try:
+                return domain.encode('idna').decode('utf-8')
+            except UnicodeError:
+                return domain
+        else:
+            return domain
+
+    def info_domain(self, domain: str, authinfo=None, clTRID=None) -> dict:
+        cmd = ec.EppInfoDomainCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = self._idna_domain(domain)
+        if clTRID is not None:
+            cmd.add_clTRID(clTRID)
+
+        if authinfo is not None:
+            cmd.authInfo = {'pw':authinfo}
+
+        return self._client_send(cmd)
+
+    def check_domain(self, domains: list[str], clTRID=None) -> dict:
+        cmd = ec.EppCheckDomainCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = [self._idna_domain(domain) for domain in domains]
+        if clTRID is not None:
+            cmd.add_clTRID(clTRID)
+        return self._client_send(cmd)
+
+    def create_domain(self, domain: str, registrant: str, host=None, description=None, secdns=None, authinfo=None) -> dict:
+        cmd = ec.EppCreateDomainCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = self._idna_domain(domain)
+        if host is not None:
+            cmd.ns = {'hostObj':host}
+        if description is not None:
+            cmd.description = description
+        cmd.registrant = registrant
+        cmd.period = {'@unit':'y', '_text':'1'}
+        if authinfo is not None:
+            cmd.authInfo = {'pw':authinfo}
+        return self._client_send(cmd)
+
+    def renew_domain(self, domain: str, curExpDate) -> dict:
+        cmd = ec.EppRenewDomainCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = self._idna_domain(domain)
+        cmd.curExpDate = curExpDate
+        cmd.period = {'@unit':'y', '_text':'1'}
+
+        return self._client_send(cmd)
+
+    def update_domain(self, domain: str, registrant=None, hosts_add=None, hosts_rem=None, add_status=None, rem_status=None, description=None,
+                      add_secdns=None, rem_secdns=None, authinfo=None) -> dict:
+        cmd = ec.EppUpdateDomainCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = self._idna_domain(domain)
+        if hosts_add is not None and len(hosts_add) > 0:
+            cmd.add = {'ns':{'hostObj':hosts_add}}
+        if hosts_rem is not None and len(hosts_rem) > 0:
+            cmd.rem = {'ns':{'hostObj':hosts_rem}}
+        if registrant is not None:
+            cmd.chg = {'registrant':registrant}
+        if description is not None:
+            cmd.chg = {'description':description}
+        if add_status is not None:
+            cmd.add = {'status':[{'@s':add_status}]}
+        if rem_status is not None:
+            cmd.rem = {'status':[{'@s':rem_status}]}
+
+        if authinfo is not None:
+            cmd.chg = {'authInfo':{'pw':authinfo}}
+
+        return self._client_send(cmd)
+
+    def delete_domain(self, domain):
+        cmd = ec.EppDeleteDomainCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = self._idna_domain(domain)
+
+        return self._client_send(cmd)
+
+    def transfer_domain(self, domain, acquirer_id, authinfo=None, op='request'):
+        extra_nsmap = copy.deepcopy(self.extra_nsmap)
+        if self.authinfo_transfer:
+            extra_nsmap['domain'] = '{}/epp/ripn-domain-1.1'.format(self.nsmap_url)
+        cmd = ec.EppTransferDomainCommand(op, extra_nsmap=extra_nsmap)
+        cmd.name = self._idna_domain(domain)
+        if op == 'request' or op == 'approve' or op == 'query':
+            if acquirer_id is not None:
+                cmd.acID = acquirer_id
+
+        return self._client_send(cmd)
+
+    def info_host(self, host):
+        cmd = ec.EppInfoHostCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = host
+        return self._client_send(cmd)
+
+    def check_host(self, host):
+        cmd = ec.EppCheckHostCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = host
+        return self._client_send(cmd)
+
+    def create_host(self, host, addr=None):
+        cmd = ec.EppCreateHostCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = host
+        if addr is not None:
+            cmd.addr = addr
+        return self._client_send(cmd)
+
+    def update_host(self, host, add_addr=None, new_name=None, rem_addr=None, add_status=None, rem_status=None):
+        cmd = ec.EppUpdateHostCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = host
+        if add_addr is not None:
+            cmd.add = {'addr':add_addr}
+        if rem_addr is not None:
+            cmd.rem = {'addr':rem_addr}
+        if new_name is not None:
+            cmd.chg = {'name':new_name}
+
+        if add_status is not None:
+            cmd.add = {'status':[{'@s':'clientUpdateProhibited'}]}
+        if rem_status is not None:
+            cmd.rem = {'status':[{'@s':'clientUpdateProhibited'}]}
+        return self._client_send(cmd)
+
+    def delete_host(self, host):
+        cmd = ec.EppDeleteHostCommand(extra_nsmap=self.extra_nsmap)
+        cmd.name = host
+        return self._client_send(cmd)
+
+    def info_registrar(self, registrar):
+        cmd = ec.EppInfoRegistrarCommand(extra_nsmap=self.extra_nsmap)
+        cmd.id = registrar
+        return self._client_send(cmd)
+
+    def update_registrar(self, registrar: str, add_addr=None, rem_addr=None) -> dict:
+        cmd = ec.EppUpdateRegistrarCommand(extra_nsmap=self.extra_nsmap)
+        cmd.id = registrar
+        cmd.chg = {}
+
+        if add_addr is not None:
+            cmd.add = {'addr':add_addr}
+
+        if rem_addr is not None:
+            cmd.rem = {'addr':rem_addr}
+
+        return self._client_send(cmd)
+
+def create_epp_client(registrar: str, passwd: str, lang: str='en', cert=None, key=None, server='127.0.0.1', port:int =700, print_commands=True) -> RipnEpp:
+    client = EppClientHTTP(port=port, host=server, ssl_certfile=cert,
+                     ssl_keyfile=key, ssl_cacerts=False, extra_nsmap={'':'http://www.ripn.net/epp/ripn-epp-1.0'})
+
+    client.connect(server, port=port)
+
+    ripn_client = RipnEpp(client=client, timing=False, print_commands=print_commands)
+
+    ret = ripn_client.login(registrar, passwd, lang=lang)
+    if ret['epp']['response']['result']['@code'] != '1000':
+        raise Exception('could not login')
+
+    return ripn_client
